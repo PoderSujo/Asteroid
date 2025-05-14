2c95b455a6dac0ae44e5cfc1c9c71618a143173d
PoderSujo <mygamescromus@yahoo.com.br>
2025-05-13 21:00:35 -0300

    3- move method

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 
 
 def main():
+    pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()  
+
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    pygame.init()
     player = Player(x, y)
-    
-    
+        
+    dt = 0
+
+
+
+
+ 
     while True:
         
         for event in pygame.event.get():
@@ -20,11 +30,22 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+
+        # Draw all objects in the drawable group
+        for entity in drawable:
+            entity.draw(screen)
+        
+        # Update all objects in the updatable group
+        updatable.update(dt)
+    
+
+
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
+        
         dt = clock.tick(60) / 1000
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
